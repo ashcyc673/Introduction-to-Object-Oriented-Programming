da925c4cbaf302c8b39cf09b1615fb17599d6d35
Chiao Ya Chang <cchang76@myseneca.ca>
2021-05-25 14:22:18 -0400

    oop_workshop

--- oop class contents/Week1/Workshop1Part2Help/Workshop1Part2Help/Marks.h
+++
@@ -1,8 +0,0 @@
-#ifndef SDDS_MARKS_H
-#define SDDS_MARKS_H
-namespace sdds {
-	void markstat(); // reads name of test, call getMarks and report
-	void getMarks(int marks[], int num); // uses getInt and getIntRange to reak #of marks and mrks required
-	void report(const char name[], int marks[], int num); // print out the report with marks
-}
-#endif
